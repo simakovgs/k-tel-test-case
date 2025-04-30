9df73a5ea2ed12bb9b3c0cb83ee021566a0eb473
simakovgs <simakov.gs@gmail.com>
2025-04-30 14:13:01 +0500

    core model small update

--- kt_test_case/apps/core/models.py
+++ kt_test_case/apps/core/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 
 class BaseAbstractModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
