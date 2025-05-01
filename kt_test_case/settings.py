1150b3d407baa5c8fa4a1019b51992363d206725
simakovgs <simakov.gs@gmail.com>
2025-05-01 18:04:41 +0500

    token auth added

--- kt_test_case/settings.py
+++ kt_test_case/settings.py
@@ -146,5 +146,23 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'kt_test_case.apps.core.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    'USE_SESSION_AUTH': False
 }
