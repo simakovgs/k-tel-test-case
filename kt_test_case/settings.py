62c8199c8e75ab95c0efe9402c2d99d147eb0666
simakovgs <simakov.gs@gmail.com>
2025-05-03 22:37:28 +0500

    added cors + settings updated

--- kt_test_case/settings.py
+++ kt_test_case/settings.py
@@ -43,19 +43,24 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'kt_test_case.apps.core',
-    'kt_test_case.apps.equipment'
+    'kt_test_case.apps.equipment',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+CORS_ALLOW_HEADERS = os.getenv('CORS_ALLOW_HEADERS', '').split(',')
 
 ROOT_URLCONF = 'kt_test_case.urls'
 
